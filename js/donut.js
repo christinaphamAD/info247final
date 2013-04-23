4623cac2c233fcf182fd45018950d549e346ff10
David Greis <dgreis@ischool.berkeley.edu>
2013-04-22 23:57:45 -0700

    Got animated donut chart working :)

--- js/donut.js
+++ js/donut.js
@@ -1,57 +1,85 @@
 // INFO 247 - Final Project
 // Donut Chart
+//if d3 not in repo, put this line in html: <script src="http://d3js.org/d3.v3.min.js"></script>
 
-
-var width = 250,
-    height = 250,
-    radius = Math.min(width, height) / 2;
-
-var color = d3.scale.ordinal()
-    .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
-
-var arc = d3.svg.arc()
-    .outerRadius(radius - 10)
-    .innerRadius(radius - 50);
-
-var pie = d3.layout.pie()
-    .sort(null)
-    .value(function(d) { return d.population; });
-
-
-//  
 
 d3.csv("js/data.csv", function(error, data) {
 
-var svg = d3.selectAll("#testd3").append("svg")
-    .attr({
-    "height": "1000px",
-    "width": "1000px"
-    })
-    .append("g")
-    .attr("transform", "translate(" + width/2 + "," + height/2 + ")");
+  var endpos = data;
+  var startpos = [0,0,0,0,0,0,1]; //todo: make this as long as data categories
 
+  var data = processData(startpos,endpos)
 
-  data.forEach(function(d) {
-    d.population = +d.population;
-    console.log(d.population)
-  });
+  var width = 160
+      height = 140
+      radius = 200
 
-  var g = svg.selectAll(".arc")
-      .data(pie(data))
-    .enter().append("g")
-      .attr("class", "arc");
+  var color = d3.scale.category20();
 
-  console.log("hi i'm here");
+  var pie = d3.layout.pie()
+      .sort(null);
 
-  g.append("path")
+  var arc = d3.svg.arc()
+      .innerRadius(radius - 100)
+      .outerRadius(radius - 60);
+
+  var svg = d3.select("#testd3").append("svg")
+      .attr("width", "100%")
+      .attr("height", "100%")
+      .append("g")
+      .attr("transform", "translate(" + width  + "," + height  + ")");
+
+  var path = svg.selectAll("path")
+      .data(pie(data.start))
+      .enter().append("path")
+      .attr("fill", function(d, i) { return color(i); })
       .attr("d", arc)
-      .style("fill", function(d) { return color(d.data.age);
-        console.log("hi i got here, my name is Earl"); });
+      .each(function(d) {
+        //console.log(this)
+        this._current = d;     // store the initial values
+        console.log(this._current)
+       // path = path.data(pie(data[this.end])); // update the data
+        //path.transition().duration(750).attrTween("d", arcTween); // redraw the arcs
+      })
+      .data(pie(data['end']))
+      .transition().duration(1500).attrTween("d", arcTween);
+      
 
-  g.append("text")
-      .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
-      .attr("dy", ".35em")
-      .style("text-anchor", "middle")
-      .text(function(d) { return d.data.age; });
+
+function processData(startpos , endpos) {
+  var endlist = makeList(endpos);
+  var map = {};
+  map['start'] = startpos;
+  map['end'] = endlist;
+  return map
+}
+
+function makeList(data){
+  var returnarr = new Array();
+  data.forEach(function(d){
+    d.population = +d.population
+    returnarr.push(d.population)
+  });
+  return returnarr
+}
+
+// Store the displayed angles in _current.
+// Then, interpolate from _current to the new angles.
+// During the transition, _current is updated in-place by d3.interpolate.
+function arcTween(a) {
+  var i = d3.interpolate(this._current, a);
+  this._current = i(0);
+  return function(t) {
+    return arc(i(t));
+  };
+}
 
 });
+
+
+
+
+
+
+
+
