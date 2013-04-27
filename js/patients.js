d44c3dee0cf70814f35a6dbe2eabc259db1c3379
Christina Pham <Christina@Christinas-MacBook-Pro.local>
2013-04-27 14:08:23 -0700

    Patient list slides down...unfancilly.

--- js/patients.js
+++ js/patients.js
@@ -30,6 +30,8 @@ function parseData(input) {
     }
     for (var k=1; k<lines.length; k++){
         $('#patientList ul').append('<li>' + k + '. <a id="' + lines[k][0] + '" data-attr="' + k + '">Patient ' + lines[k][0] + '</a></li>')
+        .hide()
+        .slideDown("slow");
     }
     console.log(lines);
     $('#patientList a').bind('click', function(e){
