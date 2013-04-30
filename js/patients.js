b4d876bb7fee7e476e7f3141dcfd94a9e45ef752
Christina Pham <Christina@Christinas-MacBook-Pro.local>
2013-04-29 22:11:32 -0700

    Patient pages updated. included some bullet-chart d3 code

--- js/patients.js
+++ js/patients.js
@@ -29,11 +29,11 @@ function parseData(input) {
         lines.push(tarr);
     }
     for (var k=1; k<lines.length; k++){
-        $('#patientList ul').append('<li>' + k + '. <a id="' + lines[k][0] + '" data-attr="' + k + '">Patient ' + lines[k][0] + '</a></li>')
+        $('#patientList ul').append('<li>' + k + '. <a id="' + lines[k][1] + '" data-attr="' + k + '">Patient ' + lines[k][1] + '</a></li>')
     }
     $('#patientList ul li').hide().each(function(e){
             //console.log(e*500)
-            $(this).delay(e*500).slideDown();
+            $(this).delay(e*350).slideDown();
     })
 //console.log(lines);
     $('#patientList a').bind('click', function(e){
@@ -45,9 +45,16 @@ function parseData(input) {
 }
 
 function getPatientData(ref, data) {
-    console.log(data[ref][0])
+    console.log(data[ref][16])
     $('#patient').append('<div id="genData" class="container"></div>')
-    $('#genData').append('<h1>Patient ' + data[ref][0] + '</h1>' )
-    .append('<h3>Gender: ' + data[ref][1] + '</h3>')
-    .append('<h3>Year of Birth: ' + data[ref][2].substr(0,1) + '/' + data[ref][2].substr(2,3) + '/' + data[ref][2].substr(4,7))
+    $('#genData').append('<h1>Patient ' + data[ref][1] + '</h1>' )
+    .append('<h3>Gender: ' + data[ref][3] + '</h3>')
+    .append('<h3>Year of Birth: ' + data[ref][4])
+    .append('<h3>Smoker: ' + data[ref][16] + '</h3>')
+
+    $('#patient').append('<div id="bulletData" class="container half left"><h2>Bullet Charts</h2></div>')
+    .append('<div id="allergies" class="container half right"><h2>Allergies</h2></div>')
+    .append('<div id="prescriptions" class="container half left"><h2>Prescriptions</h2></div>')
+    .append('<div id="diagnoses" class="container half right"><h2>Diagnoses</h2></div>')
+
 }
