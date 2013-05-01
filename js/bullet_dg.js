8fdfe534d2f4b3d31cf8ada33b119fb6719c5bc5
David Greis <dgreis@ischool.berkeley.edu>
2013-05-01 11:06:26 -0700

    adding gradients

--- js/bullet_dg.js
+++ js/bullet_dg.js
@@ -30,7 +30,7 @@ d3.csv(patientfile+"div1.csv", function(error, rawdat){
     	.enter()
     	.append('rect')
     	.attr('class','bulchart')
-    	.attr('fill','pink')
+    	.attr('fill','rgb(216, 208, 209)')
     	.attr('x',0)
     	.attr('y',function(d,i){
     		return (0 + (i)*(canvh+10)/5)
@@ -41,7 +41,7 @@ d3.csv(patientfile+"div1.csv", function(error, rawdat){
 			//console.log(d[0]);  change this to actual id 
 		});
 
-	var x = d3.scale.linear()
+	var colorscale = d3.scale.linear()
 		.domain
 
 	var charth = .05*canvh
@@ -53,7 +53,8 @@ d3.csv(patientfile+"div1.csv", function(error, rawdat){
 		.enter()
 		.append('rect')
 		.attr('class','chart')
-		.attr('fill','rgb(255, 148, 148)')
+		//.attr('fill','rgb(255, 148, 148)')
+		.attr('fill', 'url(#bgradient)')
 		.attr('height',canvh*.05)
 		//.attr('width',canvw*.9)
 		.attr('width',0)
@@ -69,13 +70,64 @@ d3.csv(patientfile+"div1.csv", function(error, rawdat){
       	.ease('linear')
       	.duration(1000)
 
+    var ngradient = canvas.append("svg:defs")
+    	.append("svg:linearGradient")
+    	.attr("id","ngradient")
+    	.attr("x1","0%")
+    	.attr("y1","0%")
+    	.attr("x2","100%")
+    	.attr("y2","0%")
+
+    ngradient.append("svg:stop")
+    	.attr("offset","0%")
+    	.attr("stop-color","#800080")
+    	.attr("stop-opacity",0.1)
+
+    ngradient.append("svg:stop")
+   		.attr("offset", "50%")
+   		.attr("stop-color", "#1f77b4")
+    	.attr("stop-opacity", 0.8);
+
+    ngradient.append("svg:stop")
+    	.attr("offset","100%")
+    	.attr("stop-color","#800080")
+    	.attr("stop-opacity",0.1)
+
+
+	var bgradient = canvas.append("svg:defs")
+  	    .append("svg:linearGradient")
+    	.attr("id",'bgradient')
+    	.attr("x1","0%")
+    	.attr("y1","0%")
+    	.attr("x2","100%")
+    	.attr("y2","0%")
+
+    bgradient.append("svg:stop")
+    	.attr("offset","0%")
+    	.attr("stop-color","rgb(255, 34, 34)")
+
+    bgradient.append("svg:stop")
+    	.data(data)
+    	.attr("offset",function(d,i){
+    		//console.log(d)
+    		return xScale(d,d[4])
+    	})
+    	.attr("stop-color","#ff9494")
+
+
+
+    
+
+
+
+
     var normbars = canvas.selectAll('chart')
     	.data(data)
     	.enter()
     	.append('rect')
     	.attr('class','normbar')
-    	.attr('fill','purple')
-    	.attr('opacity',0.4)
+    	.attr('fill','url(#ngradient)')
+    	//.attr('opacity',0.4)
     	.attr('height', charth)
     	.attr('width',0)
     	.attr('x',.08*canvw)
@@ -86,16 +138,15 @@ d3.csv(patientfile+"div1.csv", function(error, rawdat){
 		.delay(function(d,i){
 			return 0+400*i;})
 		.attr('width',function(d){
-			return (getScale(d,d[3]) - getScale(d,d[2]))
+			return (xScale(d,d[5]) - xScale(d,d[4]))
 		})
 		.attr('x',function(d){
-			console.log("hi i'm here")
-			return getScale(d,d[3])
+			return xScale(d,d[3])
 		})
 		.ease('linear')
 		.duration(750)
 
-function getScale(d,val){
+function xScale(d,val){
 	var scale = d3.scale.linear()
 		.domain([d[2],d[5]])
 		.range([.08*canvw,(.08*canvw+chartw)])
@@ -104,14 +155,7 @@ function getScale(d,val){
 
 
 
-
-
 })
- 
-
-
-
-
 
 /*
 What do I need?
@@ -124,8 +168,8 @@ function processData(rawData){
 	var data = rawData[0]
 	var ret = new Array()
 	ret.push(["BMI",+data.BMI,0,18.5,25,40])
-	ret.push(["Systolic Blood Pressure",+data.SystolicBP,50,90,140,230])
-	ret.push(["Diastolic Blood Pressure",+data.DiastolicBP,35,60,90,140])
+	ret.push(["Systolic Blood Pressure",+data.SystolicBP,50,90,140,230,120])
+	ret.push(["Diastolic Blood Pressure",+data.DiastolicBP,35,60,90,140,80])
 	ret.push(["Respiratory Rate",+data.RespiratoryRate,0,12,24,60])
 	ret.push(["Temperature",+data.Temperature,87.5,97.6,99.6,105])
 	return ret;
